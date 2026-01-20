207c7a893f9d32156e2ec77e5eed36155f0f20cd
16-Bit-Dog <aglasroth@gmail.com>
2026-01-19 20:54:46 -0500

    A new stable deque that has a reason to exist

--- deque_tests.cpp
+++ deque_tests.cpp
@@ -1,5 +1,20 @@
 #include <gtest/gtest.h>
 #include "stable_deque.h"
+#include "old_stable_deque.h"
+#include <boost/container/stable_vector.hpp>
+#include <boost/pool/pool_alloc.hpp>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <string>
+#include <array>
+#include <typeinfo>
+
+using namespace boost::container;
+using namespace std::chrono;
+using namespace boost;
 
 // Ensure gtest works
 TEST(StableDequeTest, GTest)
@@ -22,8 +37,10 @@ TEST(StableDequeTest, Smoke)
 	sd.push_front(99);
 	sd.push_back(99);
 	sd.push_back(99);
+	sd.push_back(99);
 	sd.insert(iter1 + 2, 99);
 	sd.erase(sd.begin());
+	sd.erase(sd.end()-1);
 
 	EXPECT_EQ(*iter1, 1);
 	EXPECT_EQ(*iter2, 2);
@@ -57,4 +74,120 @@ TEST(StableDequeTest, Smoke)
 
 	EXPECT_LT(iter1, iter3);
 	EXPECT_LT(iter1, iter2);
+}
+
+#define PREAMBLE(N) \
+	std::ifstream stream("magic_data.txt"); \
+	char firstChar{}; \
+	stream.read(&firstChar, 1); \
+	std::string firstCharString{}; \
+	firstCharString += firstChar; \
+	T magicData = T(std::stoi(firstCharString)); \
+	Container container{}; \
+	constexpr std::size_t count = N;
+
+#define START_PROFILE() \
+	auto start = std::chrono::high_resolution_clock::now(); 
+
+
+#define END_PROFILE() \
+	auto end = std::chrono::high_resolution_clock::now(); \
+	auto elapsed = end - start; \
+	std::cout << "(n = " << std::to_string(count) << ") " << __FUNCTION__ << "_profile<" << type_prompt << ">(...): " << elapsed.count() << '\n';
+
+template<typename T, typename Container>
+void push_front_profile(std::string type_prompt)
+{
+	PREAMBLE(5000)
+	START_PROFILE()
+	for (auto i = 0; i < count; i++)
+	{
+		container.insert(container.begin(), magicData);
+	}
+	END_PROFILE()
+}
+
+template<typename T, typename Container>
+void push_back_profile(std::string type_prompt)
+{
+	PREAMBLE(5000)
+	START_PROFILE()
+	for (auto i = 0; i < count; i++)
+	{
+		container.push_back(magicData);
+	}
+	END_PROFILE()
+}
+
+template<typename T, typename Container>
+void erase_front_profile(std::string type_prompt)
+{
+	PREAMBLE(5000)
+	for (auto i = 0; i < count; i++)
+	{
+		container.push_back(magicData);
+	}
+
+	START_PROFILE()
+	for (auto i = 0; i < count; i++)
+	{
+		container.erase(container.begin());
+	}
+	END_PROFILE()
+}
+
+template<typename T, typename Container>
+void erase_back_profile(std::string type_prompt)
+{
+	// Skip deque<T> since `end()-1` hits an assert (a MSVC bug?)
+	if constexpr (std::same_as<std::deque<T>, Container>)
+		return;
+
+	PREAMBLE(5000)
+	for (auto i = 0; i < count; i++)
+	{
+		container.push_back(magicData);
+	}
+
+	START_PROFILE()
+	for (int64_t i = count-1; i >= 0; i--)
+	{
+		container.erase(container.end() - 1);
+	}
+	END_PROFILE()
+}
+
+struct BigData
+{
+	constexpr static std::size_t size = 512;
+	std::array<int, size> data{};
+	BigData(int val)
+	{
+		std::fill(data.begin(), data.end(), val);
+	}
+};
+
+
+#define str(chars) std::string(chars)
+TEST(StableDequeTest, Perf)
+{
+#define PROFILE_FUNC(func_name, T) \
+	std::cout << "\n" << #func_name << "<" << typeid(T).name() << ">:\n"; \
+	func_name<T, std::deque<T>>(str("deque<")+str(typeid(T).name())+">"); \
+	func_name<T, stable_deque<T>>(str("stable_deque<")+str(typeid(T).name())+">"); \
+	func_name<T, old_stable_deque<T>>(str("old_stable_deque<")+str(typeid(T).name())+">"); \
+	func_name<T, std::vector<T>>("vector<"+str(typeid(T).name())+">"); \
+	func_name<T, stable_vector<T>>(str("stable_vector<")+str(typeid(T).name())+">"); 
+
+	PROFILE_FUNC(push_front_profile, int);
+	PROFILE_FUNC(push_front_profile, BigData);
+
+	PROFILE_FUNC(push_back_profile, int);
+	PROFILE_FUNC(push_back_profile, BigData);
+
+	PROFILE_FUNC(erase_front_profile, int);
+	PROFILE_FUNC(erase_front_profile, BigData);
+
+	PROFILE_FUNC(erase_back_profile, int);
+	PROFILE_FUNC(erase_back_profile, BigData);
 }
--- stable_deque.h
+++ stable_deque.h
@@ -3,262 +3,405 @@
 #include <deque>
 #include <memory>
 
-/// This is a stable deque. This is not the most efficent implementation
-/// (notice, not allocating in chunks), nor did I implement every stl method.
-/// Despite this, the implementation is complete enough to demonstrate that
-/// a stable_deque is possible and nearly the exact same as a stable_vector
-/// (node-list is backed by a deque instead of a vector), all whilst supporting
-/// a not completly atrocious O(1) random access time.
-///
-/// It is best if an arena-allocator is used with stable_deque to improve
-/// spatial locality.
+#define ASSERT_BREAK(condition) \
+	{                           \
+		if (!(condition))       \
+		{                       \
+			__debugbreak();     \
+			assert(false);      \
+		}                       \
+	}
 
-// A stable deque implementation
+/*
+What a deque is needed for:
+* fast front/back erase/insert
+* O(1) random access
+* middle insert/erase can be slow, doesn't really
+  matter since we primarily use a deque as a double
+  sided queue.
+
+This implmentation hits all of these marks... most of the time...
+
+What this implementation does that is special:
+* The core of the structure is like a boost::stable_vector
+  (indirection stores each node location)
+* The backing data structure is a deque instead of a vector
+* Instead of fixing 'up pointers' (refer to `stable_vector`'s
+  implementation), we use a 'middle' index to split the deque
+  into two halves (a left and right). This enables us to avoid
+  requiring to do an expensive iteration-pass every `push_front`.
+  This is helpful for a deque since only `.*_front`/`.*_back`
+  operations are expected to be fast (and used commonly).
+  This also means that erasing the ends is normally fast as well
+  (caveats are explained below).
+
+Limitations:
+* Inserting/erasing in the middle of the deque is O(n) due to 'up pointer' fixing
+  (but this is expected for a stable_deque/stable_vector)
+* If erasing more than a side pushes, performance degrades from O(1) to O(n)
+  due to 'up pointer' fixing starting to occur (`begin()` starts returning the starting
+  half of the 'right' side, so during every deletion we need to fix the 'up pointer' of
+  every element on the right-side).
+
+Can this be improved?
+* Probably. Removing the `if` hacks would be a good start.
+* Focusing on how to prevent the current limitation of erasure degrading to O(n) would be
+  a priority as well.
+* A .natvis to help visualize and debug the structure
+*/
+
 template <typename T, typename Allocator = std::allocator<T>>
 class stable_deque
 {
-    struct Node
-    {
-        T data;
-        // I just don't want to have to think about
-        // subtracing from size_t, so int64_t it is!
-        int64_t pos_in_nodes;
-    };
+	struct Node
+	{
+		T data;
+		int64_t pos;
+	};
 
-    using NodePAllocator = std::allocator_traits<Allocator>::template rebind_alloc<Node *>;
-    using NodePAllocatorTraits = std::allocator_traits<NodePAllocator>;
+	using NodePAllocator = std::allocator_traits<Allocator>::template rebind_alloc<Node *>;
+	using NodePAllocatorTraits = std::allocator_traits<NodePAllocator>;
 
-    using NodeAllocator = std::allocator_traits<Allocator>::template rebind_alloc<Node>;
-    using NodeAllocatorTraits = std::allocator_traits<NodeAllocator>;
+	using NodeAllocator = std::allocator_traits<Allocator>::template rebind_alloc<Node>;
+	using NodeAllocatorTraits = std::allocator_traits<NodeAllocator>;
+	NodeAllocator nodeAllocator;
 
-    using NodesDeque = std::deque<Node *, NodePAllocator>;
+	struct stable_deque_data
+	{
+		int64_t middle = -1;
 
-    class iterator
-    {
-        friend class stable_deque;
+		/// Data is stored in this order (relative to the provided iterator):
+		///[begin(), middle](middle, end())
+		std::deque<Node *, NodePAllocator> data;
+	} nodeData;
 
-        // Unlike a stable_vector, we need one additional pointer to store the deque that
-        // contains our actual `Node` data.
-        // This is required since unlike a stable_vector which has its `Node`'s stored in
-        // a contiguous vector (pointer arithmetic iterates to the next node), a stable_deque
-        // is backed by a non-contiguous container (a deque).
-        // Therefore we require an additional pointer so that we have a way to fetch "the next node".
-        NodesDeque &nodes_ref;
-        Node *node;
+	class iterator
+	{
+		friend class stable_deque;
 
-        iterator(Node *node, NodesDeque &nodes_ref) : node(node), nodes_ref(nodes_ref)
-        {
-        }
+		/// Context/parent
+		stable_deque_data &nodeDataRef;
 
-    public:
-        iterator(const iterator &iter) : nodes_ref(iter.nodes_ref), node(iter.node)
-        {
-        }
+		/// Boolean controlling if we are an iterator on the 'left' or 'right' side
+		/// (left or right is assuming stable_deque_data::data is visualized linearly)
+		bool isLeft;
 
-        T &operator*() const
-        {
-            return node->data;
-        }
+		/// Current node our iterator is operating on
+		Node *node;
 
-        // Increment / Decrement
-        iterator &operator++()
-        {
-            *this += 1;
-            return *this;
-        }
+		iterator(stable_deque_data &nodeDataRef, bool isLeft, Node *node) : nodeDataRef(nodeDataRef), isLeft(isLeft), node(node)
+		{
+		}
 
-        iterator &operator++(int)
-        {
-            *this += 1;
-            return *this;
-        }
+		decltype(stable_deque_data::data)::iterator get_underlying_data_iterator() const
+		{
+			if (isLeft)
+				return nodeDataRef.data.begin() + (nodeDataRef.middle - node->pos);
+			else
+				return nodeDataRef.data.begin() + (nodeDataRef.middle + 1 + node->pos);
+		}
 
-        iterator &operator--()
-        {
-            *this -= 1;
-            return *this;
-        }
+	public:
+		iterator(const iterator &iter) : nodeDataRef(iter.nodeDataRef), isLeft(iter.isLeft), node(iter.node)
+		{
+		}
 
-        iterator &operator--(int)
-        {
-            *this -= 1;
-            return *this;
-        }
+		T &operator*() const
+		{
+			return node->data;
+		}
 
-        T &operator[](int64_t offset) const
-        {
-            return nodes_ref[node->pos_in_nodes + offset]->data;
-        }
+		// Increment / Decrement
+		iterator &operator++()
+		{
+			*this += 1;
+			return *this;
+		}
 
-        iterator &operator+=(int64_t offset)
-        {
-            node = nodes_ref[node->pos_in_nodes + offset];
-            return *this;
-        }
+		iterator &operator++(int)
+		{
+			*this += 1;
+			return *this;
+		}
 
-        iterator &operator-=(int64_t offset)
-        {
-            *this += -offset;
-            return *this;
-        }
+		iterator &operator--()
+		{
+			*this -= 1;
+			return *this;
+		}
 
-        friend iterator operator+(const iterator &left, int64_t offset)
-        {
-            iterator tmp(left);
-            tmp += offset;
-            return tmp;
-        }
+		iterator &operator--(int)
+		{
+			*this -= 1;
+			return *this;
+		}
 
-        friend iterator operator+(int64_t offset, const iterator &right)
-        {
-            iterator tmp(right);
-            tmp += offset;
-            return tmp;
-        }
+		/// Positive offset means adding an element closer to the "right" side
+		iterator &operator+=(int64_t offset)
+		{
+			int64_t locationToIndex;
+			if (isLeft)
+			{
+				locationToIndex = node->pos - offset;
 
-        friend iterator operator-(const iterator &left, int64_t offset)
-        {
-            iterator tmp(left);
-            tmp -= offset;
-            return tmp;
-        }
+				// Switch sides
+				if (locationToIndex < 0)
+					this->isLeft = !this->isLeft;
+				this->node = nodeDataRef.data[nodeDataRef.middle - locationToIndex];
+			}
+			else
+			{
+				locationToIndex = node->pos + offset;
 
-        friend int64_t operator-(const iterator &left, const iterator &right)
-        {
-            return left.node->pos_in_nodes - right.node->pos_in_nodes;
-        }
+				// Switch sides
+				if (locationToIndex < 0)
+					this->isLeft = !this->isLeft;
+				this->node = nodeDataRef.data[nodeDataRef.middle + 1 + locationToIndex];
+			}
+			return *this;
+		}
 
-        // Comparison operators
-        friend bool operator==(const iterator &l, const iterator &r)
-        {
-            return l.node == r.node;
-        }
+		iterator &operator-=(int64_t offset)
+		{
+			*this += -offset;
+			return *this;
+		}
 
-        friend bool operator!=(const iterator &l, const iterator &r)
-        {
-            return l.node != r.node;
-        }
+		friend iterator operator+(const iterator &left, int64_t offset)
+		{
+			iterator tmp(left);
+			tmp += offset;
+			return tmp;
+		}
 
-        friend bool operator<(const iterator &l, const iterator &r)
-        {
-            return l.node->pos_in_nodes < r.node->pos_in_nodes;
-        }
+		friend iterator operator+(int64_t offset, const iterator &right)
+		{
+			iterator tmp(right);
+			tmp += offset;
+			return tmp;
+		}
 
-        friend bool operator<=(const iterator &l, const iterator &r)
-        {
-            return l.node->pos_in_nodes <= r.node->pos_in_nodes;
-        }
+		friend iterator operator-(const iterator &left, int64_t offset)
+		{
+			iterator tmp(left);
+			tmp -= offset;
+			return tmp;
+		}
 
-        friend bool operator>(const iterator &l, const iterator &r)
-        {
-            return l.node->pos_in_nodes > r.node->pos_in_nodes;
-        }
+		// Comparison operators
+		friend bool operator==(const iterator &l, const iterator &r)
+		{
+			return l.node == r.node;
+		}
 
-        friend bool operator>=(const iterator &l, const iterator &r)
-        {
-            return l.node->pos_in_nodes >= r.node->pos_in_nodes;
-        }
-    };
+		friend bool operator!=(const iterator &l, const iterator &r)
+		{
+			return l.node != r.node;
+		}
 
-    template <int howMuchToMove>
-    void fix_up_pointers(iterator iter, iterator end)
-    {
-        while (true)
-        {
-            iter.node->pos_in_nodes += howMuchToMove;
-            if (iter == end)
-                return;
-            iter++;
-        }
-    }
+		friend bool operator<(const iterator &l, const iterator &r)
+		{
+			return l.isLeft == true && r.isLeft == false || l.node->pos < r.node->pos && l.isLeft == r.isLeft;
+		}
 
-    NodeAllocator nodeAllocator;
-    NodesDeque nodes;
+		friend bool operator<=(const iterator &l, const iterator &r)
+		{
+			return l.isLeft == true && r.isLeft == false || l.node->pos <= r.node->pos && l.isLeft == r.isLeft;
+		}
 
-    void shared_init()
-    {
-        // add end node
-        Node *data = NodeAllocatorTraits::allocate(nodeAllocator, 1);
-        NodeAllocatorTraits::construct(
-            nodeAllocator,
-            data,
-            *((T *)alloca(sizeof(T))), // garbage memory
-            nodes.size());
-        nodes.push_back(data);
-    }
+		friend bool operator>(const iterator &l, const iterator &r)
+		{
+			return !(l.node->pos < r.node->pos) && l != r;
+		}
+
+		friend bool operator>=(const iterator &l, const iterator &r)
+		{
+			return !(l.node->pos < r.node->pos) || l == r;
+		}
+
+		// Other
+		T &operator[](int64_t offset) const
+		{
+			return *(*this + offset);
+		}
+	};
+
+	// Completes a `amountToShiftEachPointer` for the direction `directionToIterate`.
+	// `amountToShiftEachPointer` is inclusive to `end`.
+	template <int amountToIterateTheFixingLoop, int amountToShiftEachPointer>
+	void fix_up_pointers(iterator iter, iterator end)
+	{
+		while (true)
+		{
+			iter.node->pos += amountToShiftEachPointer;
+			if (iter == end)
+				return;
+			iter += amountToIterateTheFixingLoop;
+		}
+	}
+
+	void shared_init()
+	{
+		// Add end node
+		Node *newNode = NodeAllocatorTraits::allocate(nodeAllocator, 1);
+		NodeAllocatorTraits::construct(
+			nodeAllocator,
+			newNode,
+			*((T *)alloca(sizeof(T))), // Garbage memory
+			0);
+		nodeData.data.push_back(newNode);
+	}
+
+	enum class InsertInnerOptions
+	{
+		None,
+		ForceLeft,
+		ForceRight
+	};
+
+	// Insert on the left side of the deque
+	void insert_left(const iterator &iter, const T &value)
+	{
+		if (nodeData.middle == -1) [[unlikely]]
+		{
+			Node *newNode = NodeAllocatorTraits::allocate(nodeAllocator, 1);
+			NodeAllocatorTraits::construct(nodeAllocator, newNode, value, nodeData.middle);
+			nodeData.data.insert(iter.get_underlying_data_iterator(), newNode);
+			nodeData.middle += 1;
+			fix_up_pointers<1, 1>(iter - 1, begin());
+		}
+		else
+		{
+			Node *newNode = NodeAllocatorTraits::allocate(nodeAllocator, 1);
+			NodeAllocatorTraits::construct(nodeAllocator, newNode, value, iter.node->pos);
+			nodeData.data.insert(iter.get_underlying_data_iterator(), newNode);
+			nodeData.middle += 1;
+			fix_up_pointers<1, 1>(iter - 1, begin());
+		}
+	}
+
+	void insert_right(const iterator &iter, const T &value)
+	{
+		if (nodeData.data.back()->pos == 0) [[unlikely]]
+		{
+			Node *newNode = NodeAllocatorTraits::allocate(nodeAllocator, 1);
+			NodeAllocatorTraits::construct(nodeAllocator, newNode, value, iter.node->pos);
+			nodeData.data.insert(iter.get_underlying_data_iterator(), newNode);
+			fix_up_pointers<1, 1>(iter, end());
+		}
+		else
+		{
+			Node *newNode = NodeAllocatorTraits::allocate(nodeAllocator, 1);
+			NodeAllocatorTraits::construct(nodeAllocator, newNode, value, iter.node->pos);
+			nodeData.data.insert(iter.get_underlying_data_iterator(), newNode);
+			fix_up_pointers<1, 1>(iter, end());
+		}
+	}
+	template <InsertInnerOptions options>
+	void insert_inner(const iterator &iter, const T &value)
+	{
+		if constexpr (options == InsertInnerOptions::ForceLeft)
+		{
+			insert_left(iter, value);
+		}
+		else if constexpr (options == InsertInnerOptions::ForceRight)
+		{
+			insert_right(iter, value);
+		}
+		else
+		{
+			// Choose the side in this order:
+			// 1. If iter is `pos == 0 && middle == -1`, use left side.
+			//    This ensures that the LHS always has at-least 1 element (so that when we use
+			//    `insert(begin(), ...)` we add to the LHS).
+			// 2. If iter is not `pos == 0` (border between left/right), use the same side of `iter.isLeft`
+
+			if (iter.node->pos == 0 && nodeData.middle == -1) [[unlikely]]
+			{
+				insert_left(iter, value);
+			}
+			else
+			{
+				if (iter.isLeft)
+					insert_left(iter, value);
+				else
+					insert_right(iter, value);
+			}
+		}
+	}
 
 public:
-    stable_deque()
-    {
-        nodeAllocator = NodeAllocator();
-        nodes = NodesDeque(NodePAllocator());
-        shared_init();
-    }
-    stable_deque(const Allocator &allocator)
-    {
-        nodeAllocator = allocator;
-        nodes = NodesDeque(allocator);
-        shared_init();
-    }
-    ~stable_deque()
-    {
-        for (auto *node : nodes)
-        {
-            NodeAllocatorTraits::destroy(nodeAllocator, node);
-            NodeAllocatorTraits::deallocate(nodeAllocator, node, 1);
-        }
-        nodes.clear();
-    }
+	stable_deque()
+	{
+		nodeAllocator = NodeAllocator();
+		shared_init();
+	}
 
-    iterator begin()
-    {
-        return iterator(nodes[0], nodes);
-    }
+	~stable_deque()
+	{
+		for (auto *node : nodeData.data)
+		{
+			NodeAllocatorTraits::destroy(nodeAllocator, node);
+			NodeAllocatorTraits::deallocate(nodeAllocator, node, 1);
+		}
+	}
 
-    iterator end()
-    {
-        return iterator(nodes.back(), nodes);
-    }
+	iterator begin()
+	{
+		return iterator(nodeData, nodeData.middle != -1, nodeData.data.front());
+	}
 
-    std::size_t size()
-    {
-        // -1 for end() node
-        return nodes.size() - 1;
-    }
+	iterator end()
+	{
+		return iterator(nodeData, false, nodeData.data.back());
+	}
 
-    void push_back(const T &value)
-    {
-        insert(end(), value);
-    }
+	std::size_t size()
+	{
+		// -1 for end() node
+		return nodeData.data.size() - 1;
+	}
 
-    void push_front(const T &value)
-    {
-        Node *data = NodeAllocatorTraits::allocate(nodeAllocator, 1);
-        NodeAllocatorTraits::construct(nodeAllocator, data, value, 0);
-        nodes.insert(nodes.begin(), data);
-        fix_up_pointers<1>(begin() + 1, end());
-    }
+	void push_back(const T &value)
+	{
+		// Add to 'right' of the 'middle' of our deque
+		insert_inner<InsertInnerOptions::ForceRight>(end(), value);
+	}
 
-    void insert(iterator iterator, const T &value)
-    {
-        Node *data = NodeAllocatorTraits::allocate(nodeAllocator, 1);
-        NodeAllocatorTraits::construct(nodeAllocator, data, value, iterator.node->pos_in_nodes);
-        nodes.insert(nodes.begin() + iterator.node->pos_in_nodes, data);
-        fix_up_pointers<1>(iterator, end());
-    }
+	void push_front(const T &value)
+	{
+		// Add to 'left' of the 'middle' of our deque
+		insert_inner<InsertInnerOptions::ForceLeft>(begin(), value);
+	}
 
-    void erase(iterator iterator)
-    {
-        Node *node = iterator.node;
-        auto nextIter = iterator + 1;
-        nodes.erase(nodes.begin() + node->pos_in_nodes);
-        NodeAllocatorTraits::destroy(nodeAllocator, node);
-        NodeAllocatorTraits::deallocate(nodeAllocator, node, 1);
-        fix_up_pointers<-1>(nextIter, end());
-    }
-    T &operator[](int64_t index)
-    {
-        assert(index >= 0);
-        return nodes[index]->data;
-    }
+	void insert(iterator iterator, const T &value)
+	{
+		insert_inner<InsertInnerOptions::None>(iterator, value);
+	}
+
+	void erase(iterator iterator)
+	{
+		typename decltype(stable_deque_data::data)::iterator underlyingNode;
+		if (iterator.isLeft)
+		{
+			nodeData.middle -= 1;
+			// Since we subtract one for every iterator, we need to iterate two every loop
+			fix_up_pointers<2, -1>(iterator, begin());
+			underlyingNode = iterator.get_underlying_data_iterator();
+		}
+		else
+		{
+			underlyingNode = iterator.get_underlying_data_iterator();
+			fix_up_pointers<2, -1>(iterator, end());
+		}
+		Node *node = iterator.node;
+		nodeData.data.erase(underlyingNode);
+		NodeAllocatorTraits::destroy(nodeAllocator, node);
+		NodeAllocatorTraits::deallocate(nodeAllocator, node, 1);
+	}
+	T &operator[](int64_t index)
+	{
+		ASSERT_BREAK(index >= 0 && index < end().node->pos + nodeData.middle + 1);
+		return *(begin() + index);
+	}
 };
